cd3f1d2270f73747a06afef29d85089494f03bbb
jaxer <jaxer.27@gmail.com>
2017-11-03 15:49:33 +0800

    防止用户重复登录 增加用户退出登录代码

--- src/main/java/com/yz/zwzb/config/WebSecurityConfig.java
+++ src/main/java/com/yz/zwzb/config/WebSecurityConfig.java
@@ -1,15 +1,22 @@
 package com.yz.zwzb.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -17,26 +24,42 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/index").permitAll()
                 .anyRequest().authenticated()
                 .and()
+
                 .formLogin()
                 .loginPage("/login")
                 .permitAll()
                 .and()
-                .logout()
-                .permitAll();
+
+                .logout().permitAll().and()
+
+                .sessionManagement().maximumSessions(1).expiredUrl("/expired").maxSessionsPreventsLogin(true)
+                .sessionRegistry(sessionRegistry())
+
+        ;
+    }
+
+    // Work around https://jira.spring.io/browse/SEC-2855
+    @Bean
+    public SessionRegistry sessionRegistry()
+    {
+        SessionRegistry sessionRegistry = new SessionRegistryImpl();
+        return sessionRegistry;
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("test1").password("111111").roles("USER")
-                .and()
-                .withUser("test2").password("111111").roles("USER")
-                .and()
-                .withUser("test3").password("111111").roles("USER")
-                .and()
-                .withUser("test4").password("111111").roles("USER")
-                .and()
-                .withUser("test5").password("111111").roles("USER");
+        auth.inMemoryAuthentication().withUser("test1").password("111111").roles("USER").and().withUser("test2")
+                .password("111111").roles("USER").and().withUser("test3").password("111111").roles("USER").and()
+                .withUser("test4").password("111111").roles("USER").and().withUser("test5").password("111111").roles
+                ("USER");
+
+
+    }
+
+    // Register HttpSessionEventPublisher
+    @Bean
+    public static ServletListenerRegistrationBean httpSessionEventPublisher()
+    {
+        return new ServletListenerRegistrationBean(new HttpSessionEventPublisher());
     }
 }
